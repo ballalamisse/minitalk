4b4e4b00bd2309188352672a12da3916e6de4e9f
BALLA <lamisse.balla@gmail.com>
2026-06-30 16:20:32 +0100

    Add header file minitalk.h with function declarations

--- minitalk.h
+++ minitalk.h
@@ -1,1 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minitalk.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ballalamisse <lamisse.balla@gmail.com>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/06/30 15:33:36 by ballalamisse          #+#    #+#             */
+/*   Updated: 2026/07/01 15:35:06 by ballalamisse        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
+#ifndef MINITALK_H
+
+# define MINITALK_H
+# include <unistd.h>
+# include <stdlib.h>
+# include <signal.h>
+
+void	ft_putstr_fd(char *s, int fd);
+void	ft_putchar_fd(char c, int fd);
+int		ft_atoi(const char	*str);
+size_t	ft_strlen(const char *s);
+void	ft_putnbr_fd(int n, int fd);
+
+#endif
