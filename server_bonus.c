5f8363f3f838c5c100cfe3514f66f40d0bee6efd
BALLA <lamisse.balla@gmail.com>
2026-06-30 16:23:59 +0100

    Update server_bonus.c

--- server_bonus.c
+++ server_bonus.c
@@ -1,1 +1,61 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   server_bonus.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ballalamisse <lamisse.balla@gmail.com>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/06/30 21:25:04 by ballalamisse          #+#    #+#             */
+/*   Updated: 2026/07/01 15:32:54 by ballalamisse         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
+#include "minitalk.h"
+
+void	reset(int *a, char *b)
+{
+	*a = 0;
+	*b = 0;
+}
+
+void	sig_handler(int sig, siginfo_t *info, void *em)
+{
+	static char	c;
+	static int	bi;
+	static int	g_pid = -1;
+
+	(void)em;
+	if (g_pid != info->si_pid)
+	{
+		reset(&bi, &c);
+		g_pid = info->si_pid;
+	}
+	if (sig == SIGUSR1)
+		c = c | (c ^ (128 >> bi));
+	else if (sig == SIGUSR2)
+		c = c | (c & (128 >> bi));
+	bi++;
+	if (bi == 8)
+	{
+		write(1, &c, 1);
+		if (c == '\0')
+			kill(g_pid, SIGUSR1);
+		c = 0;
+		bi = 0;
+	}
+}
+
+int	main(void)
+{
+	struct sigaction	sac;
+
+	ft_putnbr_fd(getpid(), 1);
+	write(1, "\n", 1);
+	sac.sa_flags = SA_SIGINFO;
+	sac.sa_sigaction = sig_handler;
+	sigaction(SIGUSR1, &sac, NULL);
+	sigaction(SIGUSR2, &sac, NULL);
+	while (1)
+		usleep(5000);
+	return (0);
+}
