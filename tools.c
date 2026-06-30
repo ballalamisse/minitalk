f37aaa4c9559bcb4cdb32ad6f3b1f687903d0df0
BALLA <lamisse.balla@gmail.com>
2026-06-30 16:26:10 +0100

    Add utility functions for character, string, and number I/O

--- tools.c
+++ tools.c
@@ -1,1 +1,89 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   tools.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ballalamisse <lamisse.balla@gmail.com>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/06/30 21:29:51 by ballalamisse          #+#    #+#             */
+/*   Updated: 2026/07/01 15:33:22 by ballalamisse        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
+#include <unistd.h>
+
+void	ft_putchar_fd(char c, int fd)
+{
+	write(fd, &c, 1);
+}
+
+void	ft_putnbr_fd(int n, int fd)
+{
+	unsigned int	nbr;
+
+	nbr = n;
+	if (n < 0)
+	{
+		ft_putchar_fd('-', fd);
+		nbr = nbr * -1;
+	}
+	if (nbr < 10)
+		ft_putchar_fd(nbr + '0', fd);
+	else
+	{
+		ft_putnbr_fd(nbr / 10, fd);
+		ft_putnbr_fd(nbr % 10, fd);
+	}
+}
+
+void	ft_putstr_fd(char *s, int fd)
+{
+	if (!s)
+		return ;
+	while (*s)
+	{
+		ft_putchar_fd(*s, fd);
+		s++;
+	}
+}
+
+int	ft_atoi(const char	*str)
+{
+	long	result;
+	int		sign;
+	int		i;
+
+	i = 0;
+	sign = 1;
+	result = 0;
+	while ((str[i] >= 9 && str[i] <= 13) || str[i] == 32)
+		i++;
+	if (str[i] == '+' || str[i] == '-')
+	{
+		if (str[i] == '-')
+			sign = -1 ;
+		i++;
+	}
+	while (str[i] >= 48 && str[i] <= 57)
+	{
+		if (result > result * 10 + str[i] - '0' && sign == 1)
+			return (-1);
+		if (result > result * 10 + str[i] - '0' && sign == -1)
+			return (0);
+		result = result * 10 + str[i++] - '0';
+	}
+	return ((int)(result * sign));
+}
+
+size_t	ft_strlen(const char *s)
+{
+	size_t	l;
+
+	l = 0;
+	while (*s)
+	{
+		l++;
+		s++;
+	}
+	return (l);
+}
